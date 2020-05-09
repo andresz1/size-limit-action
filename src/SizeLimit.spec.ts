e94f39630629a82efd136e10c1a4fa56760efa3d
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2020-05-09 23:27:00 +0300

    Fix crashes due to changes in size-limit sections

--- src/SizeLimit.spec.ts
+++ src/SizeLimit.spec.ts
@@ -60,7 +60,7 @@ describe("SizeLimit", () => {
         running: 0.20210999999999999,
         loading: 2.5658984375,
         total: 2.7680084375000003
-      }
+      },
     };
 
     expect(limit.formatResults(base, current)).toEqual([
@@ -96,6 +96,66 @@ describe("SizeLimit", () => {
         "dist/index.js",
         "98.53 KB (-9.92% 🔽)"
       ]
+    ]);
+  });
+
+  test("should format size-limit with new section", () => {
+    const limit = new SizeLimit();
+    const base = {
+      "dist/index.js": {
+        name: "dist/index.js",
+        size: 110894
+      }
+    };
+    const current = {
+      "dist/index.js": {
+        name: "dist/index.js",
+        size: 100894
+      },
+      "dist/new.js": {
+        name: "dist/new.js",
+        size: 100894
+      }
+    };
+
+    expect(limit.formatResults(base, current)).toEqual([
+      SizeLimit.SIZE_RESULTS_HEADER,
+      [
+        "dist/index.js",
+        "98.53 KB (-9.92% 🔽)"
+      ],
+      [
+        "dist/new.js",
+        "98.53 KB (+100% 🔺)"
+      ],
+    ]);
+  });
+
+  test("should format size-limit with deleted section", () => {
+    const limit = new SizeLimit();
+    const base = {
+      "dist/index.js": {
+        name: "dist/index.js",
+        size: 110894
+      }
+    };
+    const current = {
+      "dist/new.js": {
+        name: "dist/new.js",
+        size: 100894
+      }
+    };
+
+    expect(limit.formatResults(base, current)).toEqual([
+      SizeLimit.SIZE_RESULTS_HEADER,
+      [
+        "dist/index.js",
+        "0 B (-100%)"
+      ],
+      [
+        "dist/new.js",
+        "98.53 KB (+100% 🔺)"
+      ],
     ]);
   });
 });
