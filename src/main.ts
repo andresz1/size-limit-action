03e5e97e7ffbbb0223ce766d5a7bc1d9e959bced
Vincent Macri <developer@vincemacri.ca>
2020-06-21 18:31:32 -0400

    Use checks instead of pull request reviews, post and update single comment instead of posting many reviews (#25)
    * Testing (#1)
    
    * Empty commit for testing
    
    * Empty commit for testing
    
    * Empty commit for testing
    
    * Dummy file for testing
    
    * Move test file
    
    * Use check instead of review
    
    * Fix error
    
    * Set limit low to test failing
    
    * Rebuild
    
    * Post comment
    
    * Make sure size doesn't fail other tests
    
    * Debug issue comments
    
    * Restore original time limit
    
    * Updates
    
    * Address feedback (#2)
    
    * Pagination (#4)
    
    * Pagination
    
    * Fix some errors
    
    * Fix pagination

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,39 @@
 import { getInput, setFailed } from "@actions/core";
 import { context, GitHub } from "@actions/github";
+
+import { Octokit } from "@octokit/rest";
+
 // @ts-ignore
 import table from "markdown-table";
 import Term from "./Term";
 import SizeLimit from "./SizeLimit";
 
 const SIZE_LIMIT_URL = "https://github.com/ai/size-limit";
+const SIZE_LIMIT_HEADING = `## [size-limit](${SIZE_LIMIT_URL}) report`;
+
+async function fetchPreviousComment(
+  octokit: GitHub,
+  repo: { owner: string; repo: string },
+  pr: { number: number }
+) {
+
+  const commentList: Octokit.IssuesListCommentsResponse = await octokit.paginate(
+    // TODO: replace with octokit.issues.listComments when upgraded to v17
+    //octokit.issues.listComments, {
+    "GET /repos/:owner/:repo/issues/:issue_number/comments",
+    {
+      ...repo,
+      // eslint-disable-next-line camelcase
+      issue_number: pr.number
+    });
+
+
+  const sizeLimitComment = commentList.find(
+    (comment: Octokit.IssuesListCommentsResponseItem) =>
+    comment.body.startsWith(SIZE_LIMIT_HEADING)
+  );
+  return !sizeLimitComment ? null : sizeLimitComment;
+}
 
 async function run() {
   try {
@@ -49,24 +77,42 @@ async function run() {
       throw error;
     }
 
-    const event = status > 0 ? "REQUEST_CHANGES" : "COMMENT";
+    if (status > 0) {
+      setFailed("Size limit has been exceeded.");
+    }
     const body = [
-      `## [size-limit](${SIZE_LIMIT_URL}) report`,
+      SIZE_LIMIT_HEADING,
       table(limit.formatResults(base, current))
     ].join("\r\n");
 
-    try {
-      octokit.pulls.createReview({
-        ...repo,
-        // eslint-disable-next-line camelcase
-        pull_number: pr.number,
-        event,
-        body
-      });
-    } catch (error) {
-      console.log(
-        "Error creating PR review. This can happen for PR's originating from a fork without write permissions."
-      );
+    const sizeLimitComment = await fetchPreviousComment(octokit, repo, pr);
+
+    if (!sizeLimitComment) {
+      try {
+        await octokit.issues.createComment({
+          ...repo,
+          // eslint-disable-next-line camelcase
+          issue_number: pr.number,
+          body
+        });
+      } catch (error) {
+        console.log(
+          "Error creating comment. This can happen for PR's originating from a fork without write permissions."
+        );
+      }
+    } else {
+      try {
+        await octokit.issues.updateComment({
+          ...repo,
+          // eslint-disable-next-line camelcase
+          comment_id: sizeLimitComment.id,
+          body
+        });
+      } catch (error) {
+        console.log(
+          "Error updating comment. This can happen for PR's originating from a fork without write permissions."
+        );
+      }
     }
   } catch (error) {
     setFailed(error.message);
